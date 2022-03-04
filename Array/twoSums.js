c111eaf6ae9419410de35932d7a714dd1edf64aa
Vaibhav04 <chawlavaibhav4@gmail.com>
2022-03-04 05:53:24 +0000

    Solved two more array problems

--- Array/twoSums.js
+++ Array/twoSums.js
@@ -1,22 +1,36 @@
+// var twoSum = function(nums, target) {
+//     const map = {}
+
+//     for(let i = 0; i < nums.length; i++) {
+//       if(map[i] !== i) {
+//         map[nums[i]] = i
+//       }
+//     }
+
+//   console.log('sds')
+
+//     for(let i = 0; i< nums.length; i++) {
+//       let value = nums[i]
+//       // For every value in nums array check if target - value exist in map?
+//       // Also check if both do not have same index
+//       // map[target - value] >= 0 (To check if this value exist in map, otherwise it would act as a falsy value)
+//         if( map[target - value] >= 0 && i !== map[target - value] ) return [map[target-value], i]
+//     }
+
+// };
+
+/* Another approach*/
 var twoSum = function(nums, target) {
-    const map = {}
-    
-    for(let i = 0; i < nums.length; i++) {
-      if(map[i] !== i) {
-        map[nums[i]] = i
-      }
-    }
+  const map = {}
 
-  console.log('sds')
-    
-    for(let i = 0; i< nums.length; i++) {
-      let value = nums[i]
-      // For every value in nums array check if target - value exist in map?
-      // Also check if both do not have same index
-      // map[target - value] >= 0 (To check if this value exist in map, otherwise it would act as a falsy value)
-        if( map[target - value] >= 0 && i !== map[target - value] ) return [map[target-value], i]
+  for (let i = 0; i < nums.length; i++) {
+    let val = (target - nums[i]);
+    if(map[nums[i]] >= 0) {
+      return [map[[nums[i]]], i]
+    } else {
+      map[val] = i;
     }
     
-};
-
+  }
+}
 console.log(twoSum([0, 1, 2, 3], 3))
